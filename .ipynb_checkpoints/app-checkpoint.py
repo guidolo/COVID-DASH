ff838d6a0c73e0c8b0d5e7f5912f6e010bc439b3
Guido sidoni <guidosidoni@Guidos-MacBook-Pro.local>
2020-03-18 22:43:03 -0300

    add deaths

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -4,44 +4,53 @@ import streamlit as st
 import altair as alt
 
 def main():
-    df = load_data()
+    df_c, df_d = load_data()
     page = st.sidebar.selectbox("Choose a page", ["Graph", "Data"])
 
     if page == "Data":
         st.header("This is your data explorer.")
         st.write("Please select a page on the left.")
         st.write(df)
+        st.write('Data from https://github.com/CSSEGISandData/COVID-19')
     elif page == "Graph":
-        st.title("Data Exploration")
-        paises = st.multiselect("Choose a Country", df.columns)
+        st.title("COVID-19 Time Exploration")
+        paises = st.multiselect("Choose a Country", df_c.columns)
         log = st.radio('Scale', ['Normal','Log'], index=0)
-        visualize_data(df, paises, log)
-
-@st.cache
-def load_data():
-    df = pd.read_csv('./data/time_series_19-covid-Confirmed.csv')
-    
+        option = st.radio('Option', ['Confirmed','Deaths'], index=0)
+        if option == 'Confirmed':
+            visualize_data(df_c, paises, log, option)
+        else:
+            visualize_data(df_d, paises, log, option)
+            
+def make_data(df):
     df.set_index(['Province/State', 'Country/Region', 'Lat', 'Long'], inplace=True)
     df = (df.
          stack().
         reset_index().
         rename(columns={'level_4':'issue_date', 0:'cant', 'Country/Region':'Country'})
     )
-
     df = df.groupby(['Country','issue_date']).sum().drop(['Lat','Long'], axis=1).reset_index()
     df.issue_date = pd.to_datetime(df.issue_date)
-
     df = df.set_index(['issue_date','Country']).unstack()
     df = df.loc[:, 'cant']
     return df
 
-def visualize_data(df, paises, log):
+@st.cache
+def load_data():
+    df_c = pd.read_csv('./data/time_series_19-covid-Confirmed.csv')
+    df_d = pd.read_csv('./data/time_series_19-covid-Deaths.csv')
+    df_c = make_data(df_c)
+    df_d = make_data(df_d)
+    return df_c, df_d
+
+def visualize_data(df, paises, log, option):
     #paises = ["Spain", "Italy", "Argentina","Japan","Germany", "Netherlands", "France", "China"]
     #paises = ["Argentina", "Chile", "Brazil", "Colombia", "Costa Rica", "Peru", "Mexico"]
     if len(paises) > 0:
         paises.sort()
         df = df.loc[:, paises]
         plt.plot(df)
+        plt.title('COVID19 -- '+option+' Cases')
         if log == 'Log':
             plt.yscale('log')
         plt.legend(paises)
