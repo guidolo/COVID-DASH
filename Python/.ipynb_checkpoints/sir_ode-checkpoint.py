5a2caba583e390d3203fe667151d45cf0c997404
Guido sidoni <guidosidoni@Guidos-MacBook-Pro.local>
2021-05-22 18:24:18 -0300

    add SIR models

--- Python/.ipynb_checkpoints/sir_ode-checkpoint.py
+++ Python/.ipynb_checkpoints/sir_ode-checkpoint.py
@@ -4,6 +4,9 @@
 
 import numpy as np
 
+
+import numpy as np
+
 def model(ini, time_step, params):
 	Y = np.zeros(3) #column vector for the state variables
 	X = ini
@@ -25,6 +28,30 @@ def x0fcn(params, data):
 
 	return X0
 
-
 def yfcn(res, params):
+	return res[:,1]*params[2]
+
+def model2(ini, t, params):
+    S, I, R = ini
+    beta = params[0]
+    gamma = params[1]
+    N = params[2]
+    dSdt = -beta*S*I/N
+    dIdt = beta*S*I/N - gamma*I
+    dRdt = gamma*I
+    return (dSdt, dIdt, dRdt)
+
+def x0fcn2(params, data):
+    S0 = 1.0 - (data[0]/params[2])
+    I0 = data[0]/params[2]
+    R0 = 0.0
+    return (S0, I0, R0)
+
+def ini2(N, I0, R0):
+    I0 = I0
+    R0 = R0
+    S0 = N - I0 - R0
+    return (S0, I0, R0)
+
+def yfcn2(res, params):
 	return res[:,1]*params[2]
